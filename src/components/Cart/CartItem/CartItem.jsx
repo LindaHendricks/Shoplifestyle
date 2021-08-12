754438ddff206ec84e1ce18e1387c1e9bbbe7422
LindaHendricks <linda.timoh@gmail.com>
2021-08-12 11:58:54 -0400

    cart button function

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@material-ui/core';
 import useStyles from './styles';
 
-const CartItem = ({item}) => {
+const CartItem = ({item, oneUpdateCartQty, onRemoveFromCart}) => {
     const classes = useStyles();
     return (
         <Card>
@@ -14,11 +14,11 @@ const CartItem = ({item}) => {
             </CardContent>
             <CardActions className={classes.CardAction}>
                 <div className={classes.buttons}>
-                    <Button type="button" size="small"> - </Button>
+                    <Button type="button" size="small" onclick={() => oneUpdateCartQty(item.id, item.quantity - 1)}> - </Button>
                     <Typography>{item.quantity}</Typography>
-                    <Button type="button" size="small"> + </Button>
+                    <Button type="button" size="small"onclick={() => oneUpdateCartQty(item.id, item.quantity + 1)}> + </Button>
                 </div>
-                <Button variant="contained" tyle="button" color="secondary">Remove</Button>
+                <Button variant="contained" tyle="button" color="secondary" onclick={() => onRemoveFromCart (item.id)}>Remove</Button>
             </CardActions>
             
         </Card>
